7cfa81dfd820c6ad83fbb77404cf0ff1c4dc9fbc
KosekiTakashi <takashi111398@gmail.com>
2020-01-30 20:56:45 +0900

    Initial Commit

---
+++ Light-Alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light-Alarm
+//
+//  Created by 小関隆司 on 2020/01/30.
+//  Copyright © 2020 kosekitakashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
