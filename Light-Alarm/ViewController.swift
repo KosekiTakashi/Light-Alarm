003a19de0f0530fb6c7bba35d5c4f2a896e12ff6
KosekiTakashi <takashi111398@gmail.com>
2020-01-30 21:20:32 +0900

    Add Code

--- Light-Alarm/ViewController.swift
+++ Light-Alarm/ViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var TimeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
