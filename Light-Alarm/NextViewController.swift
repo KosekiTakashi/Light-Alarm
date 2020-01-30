a5a036a33202e2370f7be25fe98abdd345eb0c7d
KosekiTakashi <takashi111398@gmail.com>
2020-01-30 21:07:35 +0900

    git init

---
+++ Light-Alarm/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  Light-Alarm
+//
+//  Created by 小関隆司 on 2020/01/30.
+//  Copyright © 2020 kosekitakashi. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
